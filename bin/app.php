8677474cfca0bfc7dec9d4d3fdee115a6cce6688
Fred Dysart <ftdysa@gmail.com>
2016-02-06 14:27:12 -0500

    Add a command that dumps out exif data

--- bin/app.php
+++ bin/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use Ftdysa\Website\Command\GeneratePhotoCacheCommand;
+use Ftdysa\Website\Command\ImageMetadataCommand;
 use Ftdysa\Website\Command\ResizeImagesCommand;
 use Symfony\Component\Console\Application;
 
@@ -9,4 +10,5 @@ require __DIR__.'/../vendor/autoload.php';
 $app = new Application();
 $app->add(new ResizeImagesCommand());
 $app->add(new GeneratePhotoCacheCommand());
+$app->add(new ImageMetadataCommand());
 $app->run();
